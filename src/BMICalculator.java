c176dc50cec34e1a14cae4f44c267a6adfce814f
khuzzuk <adriandrabik@gmail.com>
2016-09-08 18:22:04 +0200

    interpretacja

--- src/BMICalculator.java
+++ src/BMICalculator.java
@@ -4,59 +4,76 @@ import java.util.Scanner;
 public class BMICalculator {
     public static void main(String[] args) throws IOException {
         System.out.println("hello");
-        System.out.println("Podaj wzrost w centymetrach");
         Scanner klaw = new Scanner(System.in);
         /* do czytaniazklawiatury */
-        String wzrost = klaw.next();
-boolean poprawnyWzrost = wczytajWzrost(wzrost);
-        if (!poprawnyWzrost) return;
-        float wzrostWMetrach = Float.parseFloat(wzrost);
+        boolean test = true;
+        float wzrostWMetrach = 0;
+        while (test) {
+            System.out.println("Podaj wzrost w centymetrach");
+            String wzrost = klaw.next();
+
+            boolean poprawnyWzrost = wczytajWzrost(wzrost);
+            if (poprawnyWzrost){ test = false;
+            wzrostWMetrach = Float.parseFloat(wzrost);}
+        }
         wzrostWMetrach = wzrostWMetrach / 100;
         System.out.println("Podaj wage");
         float waga = klaw.nextInt();
 
-        float bmi = waga / (wzrostWMetrach*wzrostWMetrach); //masa/wzrost(m)^2
+        float bmi = waga / (wzrostWMetrach * wzrostWMetrach); //masa/wzrost(m)^2
 
-        System.out.println ("Twoje bmi=");
+        System.out.println("Twoje bmi=");
         System.out.println(bmi);
-
+        interpretacja(bmi);
 
 
     }
 
-    public static boolean wczytajWzrost (String wzrost)
-    {
+    public static boolean wczytajWzrost(String wzrost) {
         if (wzrost.length() != 3) {
-            System.out.println("bład"); return false;
-        }
-        if (wzrost.charAt(0)<'1' || wzrost.charAt(0)>'9'){
             System.out.println("bład");
             return false;
         }
-if (wzrost.charAt(1)>='0')
-{} else {
-    System.out.println("bład");
-} if (wzrost.charAt(1)>'9'){
-        System.out.println("bład");
-        return false;
+        if (wzrost.charAt(0) < '1' || wzrost.charAt(0) > '9') {
+            System.out.println("bład");
+            return false;
+        }
+        if (wzrost.charAt(1) >= '0') {
+        } else {
+            System.out.println("bład");
+        }
+        if (wzrost.charAt(1) > '9') {
+            System.out.println("bład");
+            return false;
+        }
+        if (wzrost.charAt(2) > '9') {
+            return false;
+        }
+        if (wzrost.charAt(2) < '0') {
+            return false;
+        }
+
+
+        return true;
+
+
     }
-    if (wzrost.charAt(2)>'9')
-    {return false;}
-    if (wzrost.charAt(2) <'0')
-    {return false;}
 
-
-return true;
-
+    public static void interpretacja(float wynik) {
+        if (wynik <= 18) {
+            System.out.println("jedz wiecej paczkow");
+        }
+        if (wynik > 18 && wynik <= 26) {
+            System.out.println("jest ok");
+        }
+        if (wynik > 26 && wynik < 30) {
+            System.out.println("za duzo paczkow");
+        }
+        if (wynik > 30) {
+            System.out.println("nie zryj tyle");
+        }
 
 
     }
-public static void interpretacja (float wynik){
-    if (wynik <= 18) {
-        System.out.println("jedz wiecej paczkow");
-    }
-if (wynik > 18 && )
-
-
-}}
+}
 
---
+++ src/test.java
@@ -0,0 +1,20 @@
+/**
+ * Created by adria on 08.09.2016.
+ */
+public class test {
+    static int x = 0;
+    public static void main(String[] args) {
+        int z = 0;
+        {
+            int y = 2 + x + z;
+            {
+                int ewa = 5 + y;
+            }
+        }
+        x = z;
+
+        if (true) {
+
+        }
+    }
+}
