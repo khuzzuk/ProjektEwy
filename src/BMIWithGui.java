d45b8738de5fbb46e400718a15e6c188a31c8b45
Ewunia <ewa.goleb@gmail.com>
2016-10-04 19:54:47 +0200

    kolkko i krzyzyk

--- src/BMIWithGui.java
+++ src/BMIWithGui.java
@@ -4,13 +4,30 @@ import java.awt.*;
 public class BMIWithGui {
     public static void main(String[] args) {
         JFrame okienkoDlaAdrianka = new JFrame();
-        FlowLayout typZmiennej = new FlowLayout();
+        GridBagConstraints ramy = new GridBagConstraints();
+        GridBagLayout layout = new GridBagLayout();
+
+        okienkoDlaAdrianka.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Container contentPane = okienkoDlaAdrianka.getContentPane();
+        contentPane.setBackground(new Color(245,247,255));
         Dimension dim = new Dimension();
-        dim.setSize(200, 400);
+        dim.setSize(800, 800);
         okienkoDlaAdrianka.setMinimumSize(dim);
-        okienkoDlaAdrianka.setLayout(typZmiennej);
-        JLabel tekst = new JLabel("tekst dla Adrianka");
+        okienkoDlaAdrianka.setLayout(layout);
+        JLabel tekst = new JLabel("Kalkulator BMI");
         okienkoDlaAdrianka.add(tekst);
+ramy.gridy = 1;
+
+        JLabel tekst2 = new JLabel("Wpisz swój wzrost");
+        okienkoDlaAdrianka.add (tekst2);
+        layout.setConstraints(tekst2,ramy);
+                JLabel tekst3 = new JLabel("Wpisz swoją wagę");
+        okienkoDlaAdrianka.add(tekst3);
+        JButton przycisk = new JButton("oblicz");
+        okienkoDlaAdrianka.add(przycisk);
+        CalculatingBMI akcjaPrzycisku = new CalculatingBMI();
+        przycisk.addActionListener(akcjaPrzycisku);
         okienkoDlaAdrianka.setVisible(true);
+
     }
 }
---
+++ src/CalculatingBMI.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+
+/**
+ * Created by Ewunia on 2016-10-04.
+ */
+public class CalculatingBMI  extends AbstractAction {
+    public void actionPerformed (ActionEvent e){
+        System.out.println("EWa jest fantastyczna!");
+    }
+
+}
